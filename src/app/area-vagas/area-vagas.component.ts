a6edbbe15ea07f668a1310edd68bb3be754f964f
PedroSalvino <pedro_luiz7199@outlook.com>
2021-11-06 01:30:09 -0300

    2 files modified

--- src/app/area-vagas/area-vagas.component.ts
+++ src/app/area-vagas/area-vagas.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { VagasService } from '../vagas.service';
 
 @Component({
   selector: 'app-area-vagas',
   templateUrl: './area-vagas.component.html',
-  styleUrls: ['./area-vagas.component.css']
+  styleUrls: ['./area-vagas.component.css'],
 })
 export class AreaVagasComponent implements OnInit {
+  pesquisa: string;
 
-  constructor() { }
+  constructor(vagas: VagasService) {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
--- src/app/vagas.service.ts
+++ src/app/vagas.service.ts
@@ -1,6 +1,43 @@
 import { Injectable } from '@angular/core';
 
+interface Vaga {
+  id: number;
+  nome: string;
+  empresa: string;
+  salario: number;
+  horario: string;
+  descricao: string;
+}
+
 @Injectable()
 export class VagasService {
-  constructor() {}
+  listvagas: Array<Vaga>;
+  constructor() {
+    this.addVaga(13, 'Designer Gráfico', 'WPK', 2200.85, 'Noturno', '');
+    this.addVaga(65, 'Assistente', 'WPK', 1989.84, 'Matutino', '');
+  }
+
+  addVaga(
+    id: number,
+    nome: string,
+    empresa: string,
+    salario: number,
+    horario: string,
+    descricao: string
+  ) {
+    this.listvagas.push({
+      id: id,
+      nome: nome,
+      empresa: empresa,
+      salario: salario,
+      horario: horario,
+      descricao: descricao,
+    });
+  }
+
+  pesquisarVaga(pesquisa: string) {
+    return this.listvagas.find((el) =>
+      el.nome.toLocaleUpperCase().includes(pesquisa.toUpperCase())
+    );
+  }
 }
