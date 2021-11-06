d3e6d3668c908dcdc8af61add9cd6385c82057b1
PedroSalvino <pedro_luiz7199@outlook.com>
2021-11-06 00:22:17 -0300

    3 files added

---
+++ src/app/confirmacao-vaga/confirmacao-vaga.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-confirmacao-vaga',
+  templateUrl: './confirmacao-vaga.component.html',
+  styleUrls: ['./confirmacao-vaga.component.css']
+})
+export class ConfirmacaoVagaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
